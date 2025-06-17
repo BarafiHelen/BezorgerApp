edc6fc7c44943f3ddf5fc889edb77403594e4a17
redroze <helen.baravi94@gmail.com>
2025-06-17 14:10:47 +0200

    views maken

--- BezorgerApp/Views/OrderDetailPage.xaml.cs
+++ BezorgerApp/Views/OrderDetailPage.xaml.cs
@@ -1,9 +1,13 @@
+using BezorgerApp.Models;
+using BezorgerApp.ViewModels;
+
 namespace BezorgerApp.Views;
 
 public partial class OrderDetailPage : ContentPage
 {
-	public OrderDetailPage()
+	public OrderDetailPage(Order selectedOrder)
 	{
 		InitializeComponent();
-	}
+        BindingContext = new OrderDetailViewModel(selectedOrder);
+    }
 }
